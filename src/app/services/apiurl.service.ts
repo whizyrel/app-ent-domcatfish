a8845d1b08c95fee8ad88d79b1755b6dacc84f17
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-09 16:20:05 +0100

    bm: convention

--- src/app/services/apiurl.service.ts
+++ src/app/services/apiurl.service.ts
@@ -10,39 +10,45 @@ export class APIURLService {
   constructor() {}
 
   get userUrls() {
-    return {
-      signup: `${this.URL}/users/signup`,
-      verify: `${this.URL}/users/verify`,
-      forgot: `${this.URL}/users/forgot`,
-      retrieve: `${this.URL}/users/retrieve`,
-      signin: `${this.URL}/users/signin`,
-      modify: `${this.URL}/users/edit`,
-      list: `${this.URL}/users/list`, // has query || params
-      details: `${this.URL}/users/details`, // has query || params
-      delete: `${this.URL}/users/delete`,
-      logout: `${this.URL}/users/logout`,
-      sessVal: `${this.URL}/val/sess`,
-    };
+    return userUrls;
   }
   get feedbackUrls() {
-    return {
-      submit: `${this.URL}/feedbacks/submit`,
-      delete: `${this.URL}/feedbacks/delete`,
-      list: `${this.URL}/feedbacks/list`,
-      'list-details': `${this.URL}/feedbacks/list-details`,
-      archive: `${this.URL}/feedbacks/archive`,
-    };
+    return feedbackUrls;
   }
   get productsUrls() {
-    return {
-      add: `${this.URL}/products/add`,
-      addPhotos: `${this.URL}/products/photos/add/`,
-      replacePhoto: `${this.URL}/products/photos/replace/`,
-      deletePhoto: `${this.URL}/products/photos/delete/`,
-      modify: `${this.URL}/products/modify/`,
-      list: `${this.URL}/products/list`,
-      details: `${this.URL}/products/details/`,
-      delete: `${this.URL}/products/delete/`,
-    };
+    return productUrls;
   }
 }
+
+const userUrls = {
+  signup: `${this.URL}/users/signup`,
+  verify: `${this.URL}/users/verify`,
+  forgot: `${this.URL}/users/forgot`,
+  retrieve: `${this.URL}/users/retrieve`,
+  signin: `${this.URL}/users/signin`,
+  modify: `${this.URL}/users/edit`,
+  list: `${this.URL}/users/list`, // has query || params
+  details: `${this.URL}/users/details`, // has query || params
+  delete: `${this.URL}/users/delete`,
+  logout: `${this.URL}/users/logout`,
+  sessVal: `${this.URL}/val/sess`,
+};
+
+const feedbackUrls = {
+  submit: `${this.URL}/feedbacks/submit`,
+  delete: `${this.URL}/feedbacks/delete`,
+  list: `${this.URL}/feedbacks/list`,
+  'list-details': `${this.URL}/feedbacks/list-details`,
+  archive: `${this.URL}/feedbacks/archive`,
+};
+
+const productsUrls = {
+  add: `${this.URL}/products/add`,
+  addPhotos: `${this.URL}/products/photos/add/`,
+  replacePhoto: `${this.URL}/products/photos/replace/`,
+  deletePhoto: `${this.URL}/products/photos/delete/`,
+  modify: `${this.URL}/products/modify/`,
+  list: `${this.URL}/products/list`,
+  details: `${this.URL}/products/details/`,
+  delete: `${this.URL}/products/delete/`,
+};
