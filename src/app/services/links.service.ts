65722b219aa4a09860a30c42199e992a36135d1a
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-09 16:27:40 +0100

    bm: convention

--- src/app/services/links.service.ts
+++ src/app/services/links.service.ts
@@ -21,117 +21,125 @@ export class LinksService {
   }
 
   getHomeNavbarLinks(): LinkProps[] {
-    return [
-      {
-        link: '',
-        title: 'Home',
-      },
-      {
-        link: 'shop',
-        title: 'Shop',
-      },
-      {
-        link: '#about-our-products',
-        title: 'Our Products',
-      },
-      {
-        link: '#contact-us',
-        title: 'Contact Us',
-      },
-    ];
+    return homeNavbarLinks;
   }
 
   get getAdminDashboardSidebarLinks(): LinkProps[] {
-    return [
-      {
-        link: '../../admin',
-        title: 'Home',
-        iconClass: 'fas fa-home mr-3'
-      },
-      {
-        link: '../../shop',
-        title: 'Shop',
-        iconClass: 'fas fa-map-marker-alt mr-3'
-      },
-      {
-        link: 'profile',
-        title: 'Profile',
-        iconClass: 'fas fa-user mr-3'
-      },
-      {
-        title: 'Products',
-        iconClass: 'fas fa-shopping-cart',
-        children: [
-            {
-            link: 'add-products',
-            title: 'add products',
-            iconClass: 'fas fa-shopping-cart',
-          },
-          {
-            link: 'list-products',
-            title: 'list products',
-            iconClass: 'fas fa-shopping-cart',
-          }
-        ]
-      },
-      {
-        title: 'Feedbacks',
-        iconClass: 'fas fa-comments mr-3',
-        children: [
-          {
-            link: 'list-feedbacks',
-            title: 'list feedbacks',
-            iconClass: 'fas fa-comments',
-          }
-        ]
-      },
-      {
-        title: 'Orders',
-        iconClass: 'fas fa-money-bill-alt mr-3',
-        children: [{
-          link: 'list-orders',
-          title: 'list orders',
-          iconClass: 'fas fa-shopping-cart',
-        }]
-      },
-      {
-        title: 'Users',
-        iconClass: 'fas fa-user mr-3',
-        children: [
-          {
-            link: 'list-users',
-            title: 'list users',
-            iconClass: 'fas fa-user',
-          },
-          {
-            link: 'edit-users',
-            title: 'edit User',
-            iconClass: 'fas fa-edit',
-          },
-          {
-            link: 'delete-user',
-            title: 'delete user',
-            iconClass: 'fas fa-trash',
-          }
-        ]
-      },
-    ];
+    return adminLinks;
   }
 
   getSocialLinks(): AllLinksProps {
-    return {
-      whatsapp: `https://whatsapp.com/`,
-      twitter: `https://twitter.com/`,
-      instagram: `https://instagram.com/`,
-      facebook: `https://facebook.com/`,
-    };
+    return socialLinks;
   }
   getFilesLinks(): AllLinksProps {
-    return {
-      imgsrc: './assets/images/tmp_bkgd.png',
-      card_1: './assets/images/card_1.png',
-      card_2: './assets/images/card_2.png',
-      card_3: './assets/images/card_3.png',
-    };
+    return fileLinks;
   }
 }
+
+const homeNavbarLinks = [
+  {
+    link: '',
+    title: 'Home',
+  },
+  {
+    link: 'shop',
+    title: 'Shop',
+  },
+  {
+    link: '#about-our-products',
+    title: 'Our Products',
+  },
+  {
+    link: '#contact-us',
+    title: 'Contact Us',
+  },
+];
+
+const adminLinks = [
+  {
+    link: '../../admin',
+    title: 'Home',
+    iconClass: 'fas fa-home mr-3'
+  },
+  {
+    link: '../../shop',
+    title: 'Shop',
+    iconClass: 'fas fa-map-marker-alt mr-3'
+  },
+  {
+    link: 'profile',
+    title: 'Profile',
+    iconClass: 'fas fa-user mr-3'
+  },
+  {
+    title: 'Products',
+    iconClass: 'fas fa-shopping-cart',
+    children: [
+        {
+        link: 'add-products',
+        title: 'add products',
+        iconClass: 'fas fa-shopping-cart',
+      },
+      {
+        link: 'list-products',
+        title: 'list products',
+        iconClass: 'fas fa-shopping-cart',
+      }
+    ]
+  },
+  {
+    title: 'Feedbacks',
+    iconClass: 'fas fa-comments mr-3',
+    children: [
+      {
+        link: 'list-feedbacks',
+        title: 'list feedbacks',
+        iconClass: 'fas fa-comments',
+      }
+    ]
+  },
+  {
+    title: 'Orders',
+    iconClass: 'fas fa-money-bill-alt mr-3',
+    children: [{
+      link: 'list-orders',
+      title: 'list orders',
+      iconClass: 'fas fa-shopping-cart',
+    }]
+  },
+  {
+    title: 'Users',
+    iconClass: 'fas fa-user mr-3',
+    children: [
+      {
+        link: 'list-users',
+        title: 'list users',
+        iconClass: 'fas fa-user',
+      },
+      {
+        link: 'edit-users',
+        title: 'edit User',
+        iconClass: 'fas fa-edit',
+      },
+      {
+        link: 'delete-user',
+        title: 'delete user',
+        iconClass: 'fas fa-trash',
+      }
+    ]
+  },
+];
+
+const socialLinks = {
+  whatsapp: `https://whatsapp.com/`,
+  twitter: `https://twitter.com/`,
+  instagram: `https://instagram.com/`,
+  facebook: `https://facebook.com/`,
+};
+
+const fileLinks = {
+  imgsrc: './assets/images/tmp_bkgd.png',
+  card_1: './assets/images/card_1.png',
+  card_2: './assets/images/card_2.png',
+  card_3: './assets/images/card_3.png',
+};
