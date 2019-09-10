21f6bc8c7a7858a4b6014a0347f597fc4c3c85c6
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-10 04:35:40 +0100

    bm: prepared input

--- src/app/product-thumbnail/product-thumbnail.component.ts
+++ src/app/product-thumbnail/product-thumbnail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ProductsProps } from '../interfaces/products-props';
 
 @Component({
   selector: 'app-product-thumbnail',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-thumbnail.component.css']
 })
 export class ProductThumbnailComponent implements OnInit {
+  @Input('list') public prodList: ProductsProps;
 
   constructor() { }
 
   ngOnInit() {
+    console.log({prod: this.prodList});
   }
 
 }
