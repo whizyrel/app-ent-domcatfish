4697b41bf53bc8571f61111317b5b128d4b63c36
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-11 02:56:49 +0100

    bm: enc_dec

--- src/app/product-fullview/product-fullview.component.ts
+++ src/app/product-fullview/product-fullview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { DecEncService } from '../services/dec-enc.service';
 
 @Component({
   selector: 'app-product-fullview',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-fullview.component.css']
 })
 export class ProductFullviewComponent implements OnInit {
+  private pid: string;
+  private seckey: string = 'app-ent-domcatfish';
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _decEnc: DecEncService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((param) => {
+      const { st } = param;
+      console.log({st});
+
+      this.pid = this._decEnc.aesDecryption(st.toString(), this.seckey);
+      console.log({pid: this.pid});
+    });
   }
 
 }
