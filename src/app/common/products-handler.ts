ac7e97296dca8e22ab4c53384d53212973599eca
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-13 14:09:57 +0100

    bm: refactored

--- src/app/common/products-handler.ts
+++ src/app/common/products-handler.ts
@@ -5,39 +5,47 @@ export class ProductsHandler {
   splitProducts(docs: ProductsProps[], size: number, cb) {
     const splittedProd = [];
     let buffer = [];
+    let err;
 
-    console.log(`[bufferer] at begining`, {buffer});
+    console.log(`[bufferer] at begining`, {buffer, docs});
 
-    docs.forEach((cur, i) => {
-       buffer.push(cur);
+    // wrapper around docs
 
-       // average case
-       if ((++i % size) === 0) {
-         console.log({i: i});
-         splittedProd.push(buffer);
-         buffer = [];
-       }
+    if (docs !== null || docs!== undefined) {
+      docs.forEach((cur, i) => {
+         buffer.push(cur);
 
-       // worst case: i + 1 % size !== 0,
-       // i === docs length || i + 1 > docs.length
-      console.log({1: (i % size) !== 0 , 2: i  === (docs.length)})
-       if (
-            (i % size) !== 0
-            && i  === (docs.length)
-          ) {
-         // console.log('less than size');
-         console.log({1: (i % size) !== 0 , 2: i  === (docs.length)});
-         splittedProd.push(buffer);
-       }
+         // average case
+         if ((++i % size) === 0) {
+           console.log({i: i});
+           splittedProd.push(buffer);
+           buffer = [];
+         }
 
-       console.log({buffer, size: size, i});
-    });
+         // worst case: i + 1 % size !== 0,
+         // i === docs length || i + 1 > docs.length
+        console.log({1: (i % size) !== 0 , 2: i  === (docs.length)})
+         if (
+              (i % size) !== 0
+              && i  === (docs.length)
+            ) {
+           // console.log('less than size');
+           console.log({1: (i % size) !== 0 , 2: i  === (docs.length)});
+           splittedProd.push(buffer);
+         }
 
-    console.log({splittedProd});
-    const err = docs.length !== 0 && splittedProd.length !== 0
+         console.log({buffer, size: size, i});
+      });
+      err = null;
+      console.log({splittedProd, err});
+      cb(err, splittedProd);
+      return;
+    }
+
+    err = docs.length !== 0 && splittedProd.length !== 0
       ? null
       : new Error('Something is wrong. list is either empty or typescript/angular is gone mad again');
-
+      console.log({splittedProd, err});
     cb(err, splittedProd);
   }
 }
