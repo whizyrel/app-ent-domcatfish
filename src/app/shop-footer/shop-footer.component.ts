8b51eb99d8e39100767137faa9d971d9eb9641c9
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-14 19:04:43 +0100

    bm: basic implementation

--- src/app/shop-footer/shop-footer.component.ts
+++ src/app/shop-footer/shop-footer.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AllLinksProps } from '../interfaces/all-links-props';
+import { LinksService } from '../services/links.service';
+
 @Component({
   selector: 'app-shop-footer',
   templateUrl: './shop-footer.component.html',
-  styleUrls: ['./shop-footer.component.css']
+  styleUrls: [
+    './css/style.min.css',
+    './css/mdb.min.css',
+    './shop.component.css',
+  ],
 })
 export class ShopFooterComponent implements OnInit {
+  public title = `Debim`.toUpperCase();
+  public soc_link: AllLinksProps;
 
-  constructor() { }
+  constructor(private _linksService: LinksService) { }
 
   ngOnInit() {
+    this.soc_link = this._linksService.getSocialLinks();
   }
 
 }
