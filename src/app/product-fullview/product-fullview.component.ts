7b1465169052fb33eaf7434f2806332b966fbad2
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-14 22:00:13 +0100

    bm: near completion

--- src/app/product-fullview/product-fullview.component.ts
+++ src/app/product-fullview/product-fullview.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { DecEncService } from '../services/dec-enc.service';
+import { ProductsService } from '../services/products.service';
+
+import { ProductsProps } from '../interfaces/products-props';
+import { HttpResponse } from '../interfaces/http-response';
 
 @Component({
   selector: 'app-product-fullview',
   templateUrl: './product-fullview.component.html',
-  styleUrls: ['./product-fullview.component.css']
+  styleUrls: [
+    './css/style.min.css',
+    './css/mdb.min.css',
+    './product-fullview.component.css',
+  ]
 })
 export class ProductFullviewComponent implements OnInit {
   private pid: string;
   private seckey: string = 'app-ent-domcatfish';
+  public info: ProductsProps;
+  private total: number = 0;
+  public availability: string;
+
+  public encURL: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private _decEnc: DecEncService
+    private _decEnc: DecEncService,
+    private _productsService: ProductsService
   ) { }
 
   ngOnInit() {
@@ -24,7 +38,25 @@ export class ProductFullviewComponent implements OnInit {
 
       this.pid = this._decEnc.aesDecryption(st.toString(), this.seckey);
       console.log({pid: this.pid});
+      this.getProductDetails(this.pid);
+      this.encURL = this._decEnc.aesEncryption(`/shop/view?st=${this.pid}`, this.seckey);
     });
   }
 
+  getProductDetails(id: string) {
+    this._productsService
+    .getProductDetails(this.pid)
+    .subscribe(
+      (data: HttpResponse) => {
+        console.log({doc: data.doc});
+        this.info = data.doc;
+        this.availability =
+        this.info.availability ? ' Available' : ' Not available'
+      },
+      (error: HttpResponse) => {
+        console.error({error});
+        // show modal
+      }
+    )
+  }
 }
