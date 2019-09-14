edbd04e80a600550c71d59b1ecfb62d413256f8f
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-14 19:20:28 +0100

    bm: footer is set

--- src/app/shop-footer/shop-footer.component.ts
+++ src/app/shop-footer/shop-footer.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AllLinksProps } from '../interfaces/all-links-props';
+import { LinkProps } from '../interfaces/link-props';
+
 import { LinksService } from '../services/links.service';
 
 @Component({
   selector: 'app-shop-footer',
   templateUrl: './shop-footer.component.html',
   styleUrls: [
+    './shop-footer.component.css',
     './css/style.min.css',
     './css/mdb.min.css',
-    './shop.component.css',
   ],
 })
 export class ShopFooterComponent implements OnInit {
   public title = `Debim`.toUpperCase();
   public soc_link: AllLinksProps;
+  public links: LinkProps[];
 
-  constructor(private _linksService: LinksService) { }
+  constructor(
+    private _linksService: LinksService,
+) { }
 
   ngOnInit() {
     this.soc_link = this._linksService.getSocialLinks();
+    this.links = this._linksService.getHomeNavbarLinks();
   }
-
 }
