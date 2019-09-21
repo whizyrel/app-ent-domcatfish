75c615bc8ebd8bba27e6fd7a1c47f801ef08cb04
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-21 20:35:20 +0100

    bm: ...

--- src/app/common/products-handler.ts
+++ src/app/common/products-handler.ts
@@ -7,34 +7,32 @@ export class ProductsHandler {
     let buffer = [];
     let err;
 
-    console.log(`[bufferer] at begining`, {buffer, docs});
+    // console.log(`[bufferer] at begining`, {buffer, docs});
 
     // wrapper around docs
-
     if (docs !== null && docs !== undefined) {
       docs.forEach((cur, i) => {
          buffer.push(cur);
 
          // average case
          if ((++i % size) === 0) {
-           console.log({i: i});
            splittedProd.push(buffer);
            buffer = [];
          }
 
          // worst case: i + 1 % size !== 0,
          // i === docs length || i + 1 > docs.length
-        console.log({1: (i % size) !== 0 , 2: i  === (docs.length)})
+        // console.log({1: (i % size) !== 0 , 2: i  === (docs.length)});
          if (
               (i % size) !== 0
               && i  === (docs.length)
             ) {
            // console.log('less than size');
-           console.log({1: (i % size) !== 0 , 2: i  === (docs.length)});
+           // console.log({1: (i % size) !== 0 , 2: i  === (docs.length)});
            splittedProd.push(buffer);
          }
 
-         console.log({buffer, size: size, i});
+         // console.log({buffer, size: size, i});
       });
       err = null;
       console.log({splittedProd, err});
