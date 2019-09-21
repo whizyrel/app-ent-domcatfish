1f3fb8b737b922688085edf69ba8350bc89c9306
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-21 23:02:06 +0100

    bm: styles imports

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: [
+    './css/mdb.min.css',
+    './css/style.min.css',
+    './checkout.component.css'
+  ]
 })
 export class CheckoutComponent implements OnInit {
 
