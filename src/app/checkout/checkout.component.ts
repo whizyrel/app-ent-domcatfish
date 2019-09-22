68b2a41cd1cc699590e490345905bade1d6eafe9
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-22 18:46:29 +0100

    bm: init cart

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, AfterContentInit, AfterContentChecked } from '@angular/core';
 
 import { CartService } from '../services/cart.service';
+import { UsersActiveInactiveService } from '../services/users-active-inactive.service';
+
 import { CartStoreProps } from '../interfaces/cart-store-props';
 import { CartProps } from '../interfaces/cart-props';
+import { SessStoreProps } from '../interfaces/sess-store-props';
 
 @Component({
   selector: 'app-checkout',
@@ -14,12 +17,16 @@ import { CartProps } from '../interfaces/cart-props';
   ]
 })
 export class CheckoutComponent implements OnInit, AfterContentInit, AfterContentChecked {
+  public cart: CartProps[];
+  private activeUser: SessStoreProps;
 
   constructor(
-    private _cartService: CartService
+    private _cartService: CartService,
+    private _users: UsersActiveInactiveService
   ) { }
 
   ngOnInit() {
+    this.activeUser = this._users.getUsersActive;
   }
 
   ngAfterContentChecked() {
@@ -30,5 +37,10 @@ export class CheckoutComponent implements OnInit, AfterContentInit, AfterContent
     setInterval(() => {}, 30000);
   }
 
-  private initCart() { }
+  private initCart() {
+    if (this.activeUser !== null && this.activeUser !== null) {
+      this.cart = this._cartService.getCartItems(this.activeUser.dt.email);
+    }
+    console.log({crt: this.cart});
+  }
 }
