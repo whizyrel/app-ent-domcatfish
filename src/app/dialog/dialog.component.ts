08b887b04998957fd79dbb95bc1248ac88c85912
Israel Oluwole <olaleyeisrael@gmail.com>
2019-09-26 09:12:16 +0100

    bm: action roundup

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
-import { InitSnackbarService } from '../services/init-snackbar.service';
-
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
@@ -13,7 +11,6 @@ export class DialogComponent {
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data,
-    private _snackBar: InitSnackbarService
   ) {}
 
   ngOnInit() { }
@@ -21,13 +18,5 @@ export class DialogComponent {
   onAction() {
     this.data.action();
     this.dialogRef.close();
-  }
-
-  onExit() {
-    console.log({i: this.data.exitInfo});
-    if (this.data.exitInfo) {
-      this._snackBar.showSnackBarFromMsg(this.data.exitInfo, 1000);
-    }
-    // this.dialogRef.close();
   }
 }
