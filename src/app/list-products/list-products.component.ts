4ff93b8f7c8835cde93014fa27d82143d2d117fd
Israel Oluwole <olaleyeisrael@gmail.com>
2019-10-06 18:23:21 +0100

    bm: basic

--- src/app/list-products/list-products.component.ts
+++ src/app/list-products/list-products.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+
+import { ProductsService } from '../services/products.service';
+
+import { ProductsProps } from '../interfaces/products-props';
+import { HttpResponse } from '../interfaces/http-response';
 
 @Component({
   selector: 'app-list-products',
@@ -6,10 +12,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-products.component.css']
 })
 export class ListProductsComponent implements OnInit {
+  public pList: ProductsProps[];
+  public displayedColumns: string[] = ['title', 'pack', 'price', 'availability', 'quantity', 'img', 'opt'];
+  public dataSource: MatTableDataSource<ProductsProps>;
 
-  constructor() { }
+ @ViewChild(MatPaginator) paginator: MatPaginator;
+ @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit() {
+  constructor(
+    private _productsService: ProductsService
+  ) { }
+
+  async ngOnInit() {
+    await this.getProductsList();
+  }
+
+  public takeID(el) {
+    console.log({el});
+  }
+
+  public applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  private getProductsList() {
+    this._productsService
+    .getProductList
+    .subscribe((data: HttpResponse) => {
+      this.pList = data.docs;
+      console.log({d: data.docs});
+      this.dataSource = new MatTableDataSource(this.pList);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+    },
+    (error: HttpResponse) => {
+      console.log({error});
+    });
   }
 
 }
