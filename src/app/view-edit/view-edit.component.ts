9322bff25c13b1df7cf4a257288b299d6d20c76f
Israel Oluwole <olaleyeisrael@gmail.com>
2019-10-07 12:56:53 +0100

    bm: basics

--- src/app/view-edit/view-edit.component.ts
+++ src/app/view-edit/view-edit.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { DecEncService } from '../services/dec-enc.service';
+import { ProductsService } from '../services/products.service';
+
+import { HttpResponse } from '../interfaces/http-response';
+import { ProductsProps } from '../interfaces/products-props';
 
 @Component({
   selector: 'app-view-edit',
   templateUrl: './view-edit.component.html',
   styleUrls: ['./view-edit.component.css']
 })
-export class ViewEditComponent implements OnInit {
+export class ViewEditComponent implements OnInit, AfterContentInit {
+  private pid: string;
 
-  constructor() { }
+  public product: ProductsProps;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _decEnc: DecEncService,
+    private _productsService: ProductsService
+  ) { }
 
   ngOnInit() {
+    this.getID();
   }
 
+  ngAfterContentInit() {
+    this.getProductDetails();
+  }
+
+  private getProductDetails() {
+    this._productsService
+    .getProductDetails(this.pid)
+    .subscribe((data: HttpResponse) => {
+      this.product = data.doc;
+    }, (error: HttpResponse) => {
+      console.log({error});
+    });
+  }
+
+  private getID() {
+    this.activatedRoute
+    .queryParams
+    .subscribe((param) => {
+      this.pid = this._decEnc.aesDecryption(param.p);
+    });
+  }
 }
