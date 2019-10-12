904d6cdd711fb8b19c72906a7001288d3fba74d4
Israel Oluwole <olaleyeisrael@gmail.com>
2019-10-12 17:27:21 +0100

    bm: guard imp

--- src/app/guards/view-edit.guard.ts
+++ src/app/guards/view-edit.guard.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot,
+  Router, ActivatedRoute
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViewEditGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+      const enc = next.queryParams.p;
+
+      // deencryption && confirming pid
+
+      if (enc !== undefined && enc !== null) {
+        return true;
+      } else {
+        this.router.navigate(['/admin', 'dashboard']);
+        return false;
+      }
   }
 }
